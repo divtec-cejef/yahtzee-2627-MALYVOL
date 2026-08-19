17998ce92148b58ea24acfa306a27ed6aed2ebb4
Malyshev Volodymyr <volodymyr.malyshev@divtec.ch>
2026-08-19 09:10:06 +0200

    Fonction to randomly get a die from 1 to 6

--- src/YahtzeeProcedural.java
+++ src/YahtzeeProcedural.java
@@ -3,5 +3,16 @@ public class YahtzeeProcedural {
     public static void main(String[] args) {
         System.out.println( "Bon courage !");
 
+        const int MIN = 1;
+        const int MAX = 6;
+        int nombre;
+
+        int tirerAleatoirement(nombre) {
+            int resultat = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
+            return resultat;
+        }
+
+        System.out.println(tirerAleatoirement(nombre)
+
     }
 }
