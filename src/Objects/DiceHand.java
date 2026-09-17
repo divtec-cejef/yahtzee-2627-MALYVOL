12aead7582f482c7bf304ae467fa924e08ec1905
Malyshev Volodymyr <volodymyr.malyshev@divtec.ch>
2026-09-17 17:17:32 +0200

    Objects Die and DiceHand are finiched

--- src/Objects/DiceHand.java
+++ src/Objects/DiceHand.java
@@ -1,4 +1,52 @@
 package Objects;
 
 public class DiceHand {
+
+    private Die[] dice = new Die[5];
+
+
+    public DiceHand() {
+        for (int i = 0; i < dice.length; i++) {
+            dice[i] = new Die();
+        }
+
+        rollAll();
+    }
+
+    public void rollAll() {
+        for (int i = 0; i < dice.length; i++) {
+            dice[i].roll();
+        }
+    }
+
+    public void reRoll(int[] positions) {
+
+        for (int position : positions) {
+            dice[position].roll();
+        }
+    }
+
+    public Die getDie(int index) {
+        return dice[index];
+    }
+
+    public int[] getValues() {
+        int[] values = new int[5];
+        for (int i = 0; i < dice.length; i++) {
+            values[i] = getDie(i).getValue();
+        }
+        return values;
+    }
+
+    public int[] getOccurances() {
+        int[] occurrences = new int[6];
+
+        for (Die die : dice) {
+            occurrences[die.getValue() - 1]++;
+        }
+
+        return occurrences;
+    }
+
+
 }
--- src/Objects/Die.java
+++ src/Objects/Die.java
@@ -1,4 +1,20 @@
 package Objects;
 
 public class Die {
+
+    private final int MIN = 1;
+    private final int MAX = 6;
+    private int value = 0;
+
+    public Die() {
+        roll();
+    }
+
+    public void roll() {
+        value = (int) (Math.floor(Math.random() * (MAX - MIN + 1)) + MIN);
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
